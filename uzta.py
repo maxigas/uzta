e8c00185d4f78e24fa7c328ff22b93c48d4b4d60
maxigas <maxigas@anargeek.net>
2014-11-30 19:26:50 +0100

    initial commit

---
+++ uzta.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+from bs4 import BeautifulSoup as bs
+from requests import get, post, Session
+from requests.utils import cookiejar_from_dict, dict_from_cookiejar
+
+url,username,password = 'http://n-1.cc/','maxigas','ALb7+/skbEvjRZV0B0a2LgNc'
+post_url = url + 'action/login'
+s = Session()
+soup = bs(s.get(url).text)
+tokens = []
+soup = soup(class_='elgg-form-login')[0]
+for x in soup.find_all('input', type='hidden'):
+    token = x['value']
+    if token != 'true':
+        tokens.append(token)
+
+data = {
+    '__elgg_token':    tokens[0],
+    '__elgg_ts':       tokens[1],
+    'username':        username,
+    'password':        password,
+    'persistent':      'true',
+    'returntoreferer': 'false' 
+   }
+
+cookies = cookiejar_from_dict(dict_from_cookiejar(s.cookies))
+print('----')
+#r = s.post(post_url, data=data, cookies=cookies, allow_redirects=True)
+r = s.post(post_url, data=data)
+print(r.text)
+print(r.headers)
+print(r.status_code)
+#print(url,username,password,tokens)
+print(cookies)
+print(post_url)
