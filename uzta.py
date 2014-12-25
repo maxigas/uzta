8ea80a574c4fa6f22ddfe07e92526d8f8e9837c3
maxigas <maxigas@anargeek.net>
2014-12-25 02:39:44 +0100

    add pager

--- uzta.py
+++ uzta.py
@@ -11,12 +11,13 @@ import os
 # ---- Step 0. Initialisation
 print('---- Step 0. Initialisation')
 def slurp(name):
-        try:
-                with open (name + '.txt', 'r') as afile:
-                        return afile.read().replace('\n', '')
-        except:
-                print('File '+name+'.txt not found.')
-                exit(1)
+	try:
+		with open (name + '.txt', 'r') as afile:
+			return afile.read().replace('\n', '')
+	except:
+		print('File '+name+'.txt not found.')
+		print('You should create it in the same directory as this script.')
+		exit(1)
 
 website,username,password = slurp('website'),slurp('username'),slurp('password')
 
@@ -52,10 +53,10 @@ s = Session()
 print('---- Step 1. Get tokens from front page')
 soup = bs(s.get(website).text)
 try:
-        tokens = [ x['value'] for x in soup(class_='elgg-form-login')[0].find_all('input', type='hidden') if x['value'] != 'true' ]
+	tokens = [ x['value'] for x in soup(class_='elgg-form-login')[0].find_all('input', type='hidden') if x['value'] != 'true' ]
 except:
-        print("Failed to log in to " + website)
-        exit(2)
+	print("Failed to log in to " + website)
+	exit(2)
         
 data = {
     '__elgg_token':    tokens[0],
@@ -80,6 +81,7 @@ group_links =      [ x[0]['href'] for x in grouplinksoup ]
 group_subs =  [ x.get_text() for x in soup(class_='elgg-subtext') ]
 groups = list(map(lambda t,l,s: {'title':t,'link':l,'sub':s},
                   group_titles,group_subs,group_links))
+
 # Example: groups → [{'title': 'Fuck Corp$urfing - FCS', 'sub': 'https://n-1.cc/g/fuck_corpsurfing_fcs', 'link': "Hack CS, don't use it! - Hackea CS, no lo uses!"}, {'title': 'LelaCoders', 'sub': 'https://n-1.cc/g/donestech+lelacoders', 'link': 'Subgroup of DonesTech'}, {'title': 'bughunting', 'sub': 'https://n-1.cc/g/bughunting', 'link': 'yo programo, tu programas, ellx hackea'}, {'title': 'B27 discussion group', 'sub': 'https://n-1.cc/g/colectivo_b27_discussion_group', 'link': 'bug = software error'}, {'title': 'Fuck FaceFuck - FFF', 'sub': 'https://n-1.cc/g/fuck-facefuck---fff', 'link': ''}, {'title': 'Help', 'sub': 'https://n-1.cc/g/help', 'link': "Hack FB, don't use it! - Hackea facebook! No lo uses!"}, {'title': 'HackTheNight', 'sub': 'https://n-1.cc/g/hackthenight', 'link': 'helping to start in Lorea'}, {'title': 'HackLabs', 'sub': 'https://n-1.cc/g/hacklabs', 'link': 'hackspace @ cso la otra carboneria'}]
 
 # data structure:
@@ -103,52 +105,71 @@ gid = disc_url.split('/')[-1] # E.g. the last word of the URL.
 
 # ---- Step 5. Goto groups page and get all the discussions
 print('---- Step 5. Goto groups page and get all the discussions')
-soup = bs(s.get(disc_url).text)
-entry = {'title':   "x",
-         'link':    "x",
-         'first':   "x",
-         'author':  "x",
-         'last':    "x",
-         'replier': "x",
-         'count':   "x",
-         'tags':    "x",
-         'content': "x"}
-entry_titles = [ x.string for x in soup.select('h3 a') ]
-entry_links = [ x['href'] for x in soup.select('h3 a') ]
-entry_firsts = [ x.time['datetime'] for x in soup.select('.elgg-subtext') if x.time]
-entry_authors = [ x.a.string for x in soup.select('.elgg-subtext') if 'Started' in x.contents[0].string ]
-entry_lasts = [ x.time['datetime'] if x.time else 'n/a' for x in soup.select('.groups-latest-reply') ]
-entry_repliers = [ x.a.string if x.a else 'n/a' for x in soup.select('.groups-latest-reply') ]
-entry_counts = [ search(r'[0-9]+', x.find_all('a')[1].string).group() if n != 'n/a' else '0' for x,n in zip(soup.select('.elgg-subtext'), entry_lasts) ]
-entry_tags = []
-for body in soup.select('.elgg-body'):
-        if body.select('.clearfix'):
-                entry_tags.append([ tag.string for tag in body.select('.elgg-tag') ])
-        else:
-                entry_tags.append([])
-entry_contents = [ x.string for x in soup.select('.elgg-content') ]
-entries = list(map(lambda tit,lin,fir,aut,las,rep,cou,tag,con: {'title':tit,
-                                                     'link':lin,
-                                                     'first':fir,
-                                                     'author':aut,
-                                                     'last':las,
-                                                     'replier':rep,
-                                                     'count':cou,
-                                                     'tags':tag,
-                                                     'content':con},
-                 entry_titles,
-                 entry_links,
-                 entry_firsts,
-                 entry_authors,
-                 entry_lasts,
-                 entry_repliers,
-                 entry_counts,
-                 entry_tags,
-                 entry_contents))
-print(entries[0])
-# TODO Pager!!!
 
-# ---- Step 6. Save some content
+def read_discussions_page(soup):
+	""" Take soup and return discussions from discussion page. soup => entries """
+	entry = {'title':   "x",
+			 'link':    "x",
+             'first':   "x",
+             'author':  "x",
+             'last':    "x",
+             'replier': "x",
+             'count':   "x",
+             'tags':    "x",
+             'content': "x"}
+	entry_titles = [ x.string for x in soup.select('h3 a') ]
+	entry_links = [ x['href'] for x in soup.select('h3 a') ]
+	entry_firsts = [ x.time['datetime'] for x in soup.select('.elgg-subtext') if x.time]
+	entry_authors = [ x.a.string for x in soup.select('.elgg-subtext') if 'Started' in x.contents[0].string ]
+	entry_lasts = [ x.time['datetime'] if x.time else 'n/a' for x in soup.select('.groups-latest-reply') ]
+	entry_repliers = [ x.a.string if x.a else 'n/a' for x in soup.select('.groups-latest-reply') ]
+	entry_counts = [ search(r'[0-9]+', x.find_all('a')[1].string).group() if n != 'n/a' else '0' for x,n in zip(soup.select('.elgg-subtext'), entry_lasts) ]
+	entry_tags = []
+	for body in soup.select('.elgg-body'):
+		if body.select('.clearfix'):
+			entry_tags.append([ tag.string for tag in body.select('.elgg-tag') ])
+		else:
+			entry_tags.append([])
+			entry_contents = [ x.string for x in soup.select('.elgg-content') ]
+	return list(map(lambda tit,lin,fir,aut,las,rep,cou,tag,con:
+					 {'title':tit,
+					  'link':lin,
+					  'first':fir,
+					  'author':aut,
+					  'last':las,
+					  'replier':rep,
+					  'count':cou,
+					  'tags':tag,
+					  'content':con},
+					 entry_titles,
+					 entry_links,
+					 entry_firsts,
+					 entry_authors,
+					 entry_lasts,
+					 entry_repliers,
+					 entry_counts,
+					 entry_tags,
+					 entry_contents))
+
+def read_discussions(url):
+	""" Take soup and return discussions from discussion page. soup => entries """
+	soup = bs(s.get(url).text)
+	pager_links = [ x['href'] for x in soup.select('.elgg-pagination a')[:-1] ]
+	entries = []
+	for u in [url] + pager_links:
+		print('Reading ' + u)
+		entries = entries + read_discussions_page(bs(s.get(u).text))
+	return entries
+
+entries = read_discussions(disc_url)
+
+# ---- Step 6. Goto groups page and get all the tasks
+print('---- Step 6. Goto groups page and get all the tasks')
+
+# TODO 
+
+# ---- Step 7. Save some content
+print('---- Step 7. Save some content')
 def mkdir(dir_name):
         if not os.path.exists(dir_name):
             os.makedirs(dir_name)
@@ -156,16 +177,22 @@ def mkdir(dir_name):
 mkdir(output_dir_name)
 mkdir(os.path.join(output_dir_name, 'testgroup'))
 mkdir(os.path.join(output_dir_name, 'testgroup', 'discussion'))
-filebasename = os.path.join(output_dir_name, 'testgroup', 'discussion', 'discussion')
 
-for entry,n in zip(entries,range(len(entries))):
-        lines = [ key+': '+value for key, value in entry.items() if not isinstance(value,list)]
-        filename = filebasename+str(n)+'.txt'
-        print(filename)
-        with open (filename, 'w') as fileio:
-                for line in lines:
-                        print(line,file=fileio)
-                        
+def write_discussions(entries):
+	filebasename = os.path.join(output_dir_name,
+								'testgroup',
+								'discussion',
+								'discussion')
+	for entry,n in zip(entries,range(len(entries))):
+		lines = [ key+': '+value for key, value in entry.items() if not isinstance(value,list)]
+		filename = filebasename+str(n)+'.txt'
+		print(filename)
+		with open (filename, 'w') as fileio:
+			for line in lines:
+				print(line,file=fileio)
+
+write_discussions(entries)
+
 # Group ontology:
 # 
 # [ ] Group assemblies
